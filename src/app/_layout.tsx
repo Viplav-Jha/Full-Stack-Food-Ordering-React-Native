99a5c7cf6f30c2fbe431786e38731ed3f794f5ef
Viplav jha <viplavjha1993@gmail.com>
2024-02-10 09:54:08 +0530

    UI Development

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import CartProvider from '@/Provider/CartProvider';
 
 import { useColorScheme } from '../components/useColorScheme';
 
@@ -49,10 +50,13 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+     <CartProvider>
+     <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
       </Stack>
+     </CartProvider>
+      
     </ThemeProvider>
   );
 }
