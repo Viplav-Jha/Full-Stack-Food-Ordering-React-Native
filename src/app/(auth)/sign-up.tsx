fd9ed50103b821fbbfa822b5ad224f0a9b18c7d0
Viplav jha <viplavjha1993@gmail.com>
2024-02-14 18:36:19 +0530

    UI_development

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,14 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
+import React, { useState } from 'react';
+import Button from '../../components/Button';
+import Colors from '../../constants/Colors';
+import { Link, Stack } from 'expo-router';
 
-const Signup = () => {
+
+const SignUpScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    // const { error } = await supabase.auth.signUp({ email, password });
+
+    // if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Sign up' }} />
+
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder=""
+        style={styles.input}
+        secureTextEntry
+      />
+
+      <Button
+        onPress={signUpWithEmail}
+        disabled={loading}
+        text={loading ? 'Creating account...' : 'Create account'}
+      />
+      <Link href="/sign-in" style={styles.textButton}>
+        Sign in
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default Signup
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent: 'center',
+    flex: 1,
+  },
+  label: {
+    color: 'gray',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  textButton: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default SignUpScreen;
